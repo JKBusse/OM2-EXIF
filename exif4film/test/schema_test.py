c4f1830b33bddb8b93fd1540f297072ea6185b8d
Maximaleffekt <binarydreamer@protonmail.com>
2025-05-29 10:27:10 +0200

    added unit test for invalid json file

--- exif4film/test/schema_test.py
+++ exif4film/test/schema_test.py
@@ -1,14 +1,18 @@
-import unittest
 import json
+import unittest
+
 from jsonschema import validate, ValidationError, SchemaError
+
+SCHEMA_JSON = '../exif4film.schema.json'
+BASIC_FILM = './resources/basic_film.json'
+BROKEN_FILM = './resources/broken_film.json'
 
 
 class SchemaTests(unittest.TestCase):
     def test_schema_validation(self):
-        # Load the schema
-        with open('../exif4film.schema.json', 'r') as schema_file:
+        with open(SCHEMA_JSON, 'r') as schema_file:
             schema = json.load(schema_file)
-        with open('./resources/basic_film.json', 'r') as basic_film_file:
+        with open(BASIC_FILM, 'r') as basic_film_file:
             basic_film = json.load(basic_film_file)
 
         self.assertTrue(schema, "Schema should not be empty")
@@ -19,6 +23,18 @@ class SchemaTests(unittest.TestCase):
             self.fail(f"Schema validation failed: {e.message}")
         except SchemaError as e:
             self.fail(f"Schema is invalid: {e.message}")
+
+    def test_invalid_film_file(self):
+        with open(SCHEMA_JSON, 'r') as schema_file:
+            schema = json.load(schema_file)
+        with open(BROKEN_FILM, 'r') as broken_film_file:
+            broken_film = json.load(broken_film_file)
+
+        self.assertTrue(schema, "Schema should not be empty")
+        self.assertTrue(broken_film, "Basic film data should not be empty")
+
+        with self.assertRaises(ValidationError):
+            validate(broken_film, schema)
 
 
 if __name__ == '__main__':
